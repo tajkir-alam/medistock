4cb61675cc2c8d75d25478854deafce0a73e9ca1
tajkir-alam <tajkir.alam@gmail.com>
2026-05-15 00:35:51 +0600

    first commit

---
+++ src/main/java/com/medistock/inventory/MedistockApplication.java
@@ -0,0 +1,13 @@
+package com.medistock.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedistockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedistockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medistock/inventory/MedistockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medistock.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedistockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
